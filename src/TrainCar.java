76722dfcca107efc9b37388767a9b07ba04759cc
MukhammadBeisekenov <muhammadbeisekenov@gmail.com>
2024-01-22 20:01:25 +0600

    hi my 2 assignment updated;

--- src/TrainCar.java
+++ src/TrainCar.java
@@ -32,11 +32,19 @@ class FreightCar extends TrainCar {
 }
 
 
-class Train {
+ class Train {
     private TrainCar[] car;
+    private int numberofpassengers;
 
-    public Train(TrainCar[] cars) {
+    public Train(TrainCar[] cars,  int numberofpassengers ) {
         this.car = cars;
+        this.numberofpassengers = numberofPassengers;
+    }
+    public int getnumberofpassengers(){
+        return  numberofpassengers;
+    }
+    public void setNumberofpassengers(int numberofpassengers){
+        this.numberofpassengers = numberofpassengers;
     }
 
 
@@ -55,12 +63,20 @@ class TrainAndCar {
         PassengerCar passengerCar1 = new PassengerCar(68);
         PassengerCar passengerCar2 = new PassengerCar(90);
         FreightCar freightCar1 = new FreightCar(185);
-
+        Train passengercar = new Train(50);
 
         TrainCar[] trainCars = {locomotive, passengerCar1, passengerCar2, freightCar1};
         Train myTrain = new Train(trainCars);
 
+        int passengerInpassengercar = passengercar.getnumberofpassengers();
+        System.out.println("Количестово пассажирова в вагоне :" + passengerInpassengercar);
+        passengercar.setNumberofpassengers(90);
+        passengerInpassengercar = passengercar.getnumberofpassengers();
+        System.out.println("Обновление количество пассажиров в пассажирском вагоне" + passengerInpassengercar);
 
         System.out.println("Total Carrying Capacity of the Train: " + myTrain.SumCap());
     }
 }
+
+
+
